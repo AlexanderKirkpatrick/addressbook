a11368bfbda9f848cda5ba9196b4ad12206bf67d
Alexander <knibz@protonmail.com>
2022-04-24 14:32:16 -0700

    add assignId method, edit addContact to accept assignId

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
+// Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = {};
+  this.currentId = 0;
 }
 
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+}
+
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
