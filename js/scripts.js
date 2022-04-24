08a6cf95f93464c0d8010f9475fb2bb949e9df81
Alexander <knibz@protonmail.com>
2022-04-24 16:40:54 -0700

    add commented out update function

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,31 @@ function AddressBook() {
   this.currentId = 0;
 }
 
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+}
+
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
 
-AddressBook.prototype.addContact = function(contact) {
-  contact.id = this.assignId();
-  this.contacts[contact.id] = contact;
-}
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
 
 // Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
@@ -24,3 +40,7 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
  }; 
+
+ /*Contact.prototype.update = function() {
+  let contact = new Contact()
+ };*/ 
